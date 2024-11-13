9fd7e88f20165b45186bc731e27b985654c0d0f4
Brian Martins2006 <brianmartins997232779@gmail.com>
2024-11-13 09:41:21 -0300

    Teste

---
+++ Lista_de_Exercícios 2 – Matrizes/Exercicio_8.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+   internal class Exercicio_8
+    {
+        public static void mostra(int[,] mat)
+        {
+            int linhas = mat.GetLength(0);
+            int colunas = mat.GetLength(1);
+            int cont = 1;
+
+            for (int i = 0; i < linhas; i++)
+            {
+                Console.Write($"Região {cont}: ");
+                for (int j = 0; j < colunas; j++)
+                {
+                    Console.Write($" {mat[i, j]} |");
+                }
+                cont++;
+                Console.WriteLine();
+            }
+        }
+
+        static void Main()
+        {
+            int[,] matrix = new int[5, 8];
+            Random random = new Random();
+
+            // Populando a matriz com 1 em uma posição aleatória
+            matrix[random.Next(0, 5), random.Next(0, 8)] = 1;
+
+            int n = 0;
+            do
+            {
+                Console.WriteLine("Digite um número entre 2 e 500000:");
+                n = int.Parse(Console.ReadLine());
+            } while (n < 2 || n > 500000);
+
+            mostra(matrix);
+
+            while (n > 0)
+            {
+                Console.WriteLine("Digite a posição x:");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a posição y:");
+                int y = int.Parse(Console.ReadLine());
+
+                if (x >= 0 && x < 5 && y >= 0 && y < 8) // Verificação para garantir que os índices estão dentro do limite
+                {
+                    if (matrix[x, y] == 1)
+                    {
+                        Console.WriteLine("1");
+                    }
+                    else
+                    {
+                        Console.WriteLine("0");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Posição fora dos limites da matriz.");
+                }
+
+                n--;
+            }
+
+            if (n == 0)
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+
---
+++ Lista_de_Exercícios 2 – Matrizes/Exercicio_9.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Exercicio_9
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        // Determinar os limites máximos do plano cartesiano
+        int maxX = 1001; // Ajustado para garantir que os índices estejam dentro do limite
+        int maxY = 1001;
+
+        // Matriz para representar o plano do mar
+        bool[,] mar = new bool[maxX, maxY];
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] entrada = Console.ReadLine().Split();
+            int x1 = int.Parse(entrada[0]);
+            int y1 = int.Parse(entrada[1]);
+            int x2 = int.Parse(entrada[2]);
+            int y2 = int.Parse(entrada[3]);
+
+            // Marcar os pontos cobertos pelo retângulo (x1, y1) até (x2, y2)
+            for (int x = x1; x < x2; x++)
+            {
+                for (int y = y1; y < y2; y++)
+                {
+                    mar[x, y] = true; // Marca o ponto como coberto
+                }
+            }
+        }
+
+        // Contar quantos pontos estão cobertos
+        int areaCoberta = 0;
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                if (mar[x, y])
+                {
+                    areaCoberta++;
+                }
+            }
+        }
+
+        Console.WriteLine(areaCoberta);
+    }
+}
